f386a2c1805dae81d76805fc359ab92900b799d5
Doleshor khadka <39978065+SUNILkhadka@users.noreply.github.com>
2021-11-25 10:49:37 -0500

    Delete EvotingSystem directory

--- EvotingSystem/migrations/2_initial_migration.js
+++
@@ -1,8 +0,0 @@
-const Echunab = artifacts.require("Evoting");
-// const Temp = artifacts.require("./Temp.sol")
-module.exports = function (deployer) {
-  deployer.deploy(Echunab);
-};
-// module.exports = function (deployer) {
-//   deployer.deploy(Temp)
-// };
